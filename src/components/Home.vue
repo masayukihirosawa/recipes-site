c26fb33b76d65bb92e07dae521c3d8feb15ebd22
m.hirosawa <m.hirosawa0228@gmail.com>
2021-02-23 15:30:23 +0900

    v-forでレシピ一覧を表示させることまでは出来たが、レシピごとリンクを設定が出来なかったのでやめた

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <h2>レシピ一覧</h2>
-    <ul>
-      <div v-for="(content, key) in contents" :key="key">
-        <template>
-          <li>{{ content.title }}</li>
-          <li>{{ content.date }}</li>
-          <img :src="content.img" alt="料理の写真" class="photo" />
-        </template>
-      </div>
-      <!-- <li @click="toPost1">{{ contents.Post1.title }}</li> -->
-      <!-- <img :src="contents.Post1.img" alt="料理の写真" class="photo" /> -->
-      <!-- <li @click="toPost2">ほうれん草のレシピ</li>
-      <li @click="toPost3">菊菜のレシピ</li> -->
+    <ul @click="toPost1">
+      <li>{{ contents.Post1.title }}</li>
+      <li>{{ contents.Post1.date }}</li>
+      <img :src="contents.Post1.img" alt="料理の写真" class="photo" />
+    </ul>
+    <ul @click="toPost2">
+      <li>{{ contents.Post2.title }}</li>
+      <li>{{ contents.Post2.date }}</li>
+      <img :src="contents.Post2.img" alt="料理の写真" class="photo" />
+    </ul>
+    <ul @click="toPost3">
+      <li>{{ contents.Post3.title }}</li>
+      <li>{{ contents.Post3.date }}</li>
+      <img :src="contents.Post3.img" alt="料理の写真" class="photo" />
     </ul>
   </div>
 </template>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <router-view>
-      <!-- <template #title="slotProps">
-        <h2>{{ slotProps.contents.Post1.title }}</h2>
-        <p>{{ slotProps.contents.Post1.date }}</p>
-        <img :src="slotProps.contents.Post1.img" alt="料理の写真" class="photo" />
-      </template> -->
       <template #recipe>
         <h3>つくり方</h3>
         <hr />
